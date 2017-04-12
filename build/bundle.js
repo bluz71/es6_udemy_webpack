3b2cfb6fe5fb3388272edf04f7b9b5eacb0581ce
Dennis B <bluz71@users.noreply.github.com>
2017-04-12 18:29:39 +1000

    Got it working on Linux.

--- build/bundle.js
+++ build/bundle.js
@@ -63,21 +63,14 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
-
-console.log("Hello world! (from Webpack)");
-
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(0);
+(function webpackMissingModule() { throw new Error("Cannot find module \"./app/index.js\""); }());
 
 
 /***/ })
